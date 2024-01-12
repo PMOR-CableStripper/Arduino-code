5267123b3d123baaacb1af5f5000eebf627487be
CasperdeRuiter <135846353+CasperdeRuiter@users.noreply.github.com>
2024-01-12 13:00:52 +0100

    Update LCD_met_I2C_test.ino

--- LCD_met_I2C_test.ino
+++ LCD_met_I2C_test.ino
@@ -1,1 +1,20 @@
-LCD_met_I2C_test.ino
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+//-----Hardware Adressierung-----
+//Bei falscher Funktion bitte obere Zeile auskommentieren,
+//und untere Zeile freigeben
+LiquidCrystal_I2C lcd(0x27,20,4);
+//LiquidCrystal_I2C lcd(0x27,20,4);
+void setup()
+{
+ lcd.init();
+}
+void loop()
+{
+ lcd.backlight();
+ //Nachricht ausgeben
+ lcd.setCursor(0,0);
+ lcd.print(" Rij 1");
+ lcd.setCursor(0,1);
+ lcd.print(" Rij 2");
+}
